49fcfc851a3bbe2c37847aefe9515b294a51625b
Cris-Mur <yomy98@gmail.com>
2020-06-26 17:59:01 -0500

    [3 y 4] faltaría 5 storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -6,30 +6,20 @@ import datetime
 
 class BaseModel:
     """ Class base """
-    def __init__(self, id=None, name=None, my_number=None, created_at=None, updated_at=None):
-        """ ID setter """
-        if id is not None:
-            self.id = id
+    def __init__(self, *args, **kwargs):
+        if args != ():
+            if args[0] is not None and type(args[0]) == str:
+                self.id = args[0]
+            if args[1] is not None and type(args[1]) == str:
+                self.created_at = datetime.datetime.fromisoformat(args[1])
+        elif kwargs != {}:
+            if kwargs["id"] is not None:
+                self.id = kwargs["id"]
+            if kwargs["created_at"] is not None:
+                self.created_at = datetime.datetime.fromisoformat(kwargs["created_at"])
         else:
             self.id = str(uuid.uuid4())
-
-        if name is not None:
-            self.name = name
-        if my_number is not None:
-            self.my_number = my_number
-        if created_at is not None:
-            self.created_at = created_at
-        else:
             self.created_at = datetime.datetime.now()
-        if updated_at is not None:
-            self.updated_at = updated_at
-        else:
-            self.updated_at = datetime.datetime.now()
-
-    def created_at(self):
-        self.created_at = datetime.datetime.now()
-
-    def updated_at(self):
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
@@ -40,7 +30,11 @@ class BaseModel:
         """ save object to storage """
         self.updated_at = datetime.datetime.now()
         return self.updated_at
-        #updated_at()
+
     def to_dict(self):
         """ to dict """
-        return self.__dict__
+        ret = self.__dict__.copy()
+        ret["__class__"] = self.__class__.__name__
+        ret["updated_at"] = self.updated_at.isoformat()
+        ret["created_at"] = self.created_at.isoformat()
+        return ret
