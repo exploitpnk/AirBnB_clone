14d65a0bd1e8f5d4d4c86d65b8be79882225f27e
Cris-Mur <yomy98@gmail.com>
2020-06-29 13:16:58 -0500

    [add create function in console]

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """ Simple command interpreter """
@@ -12,10 +14,23 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """ Ctrl+D to exit the programm """
         return True
-    
+
     def emptyline(self):
         """ Empty line """
         pass
+
+
+    def do_create(self, arg):
+        """ Creates a new instance of (arg) """
+        if not arg:
+            print("** class name missing **")
+            return None
+        try:
+            mo = eval(arg + "()")
+            mo.save()
+            print(mo.id)
+        except:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
