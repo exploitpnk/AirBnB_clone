8a4bd04e46492d0f711cf5fb3d8bb3b5177e039b
exploitpnk <exploitpnk@gmail.com>
2020-07-02 10:09:36 -0500

    [update] commands added

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Console - Simple command interpreter for Airbnb"""
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -26,21 +27,20 @@ class HBNBCommand(cmd.Cmd):
         """ Empty line """
         pass
 
-
     def do_create(self, arg):
-        """ Creates a new instance of (arg) """
+        """ Creates a new instance """
         if not arg:
-            print("** class name missing **")
+            print('** class name missing **')
             return None
         try:
-            mo = eval(arg + "()")
-            mo.save()
-            print(mo.id)
+            model = eval(arg + "()")
+            model.save()
+            print(model.id)
         except:
             print("** class doesn't exist **")
 
     def do_show(self, arg):
-        """ Print instances from id """
+        """ Show instance dictorinary based in ID """
         argv = arg.split()
         if not argv:
             print("** class name missing **")
@@ -51,7 +51,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return None
 
-        al = storage.all()
+        objects = storage.all()
 
         if len(argv) < 2:
             print("** instance id missing **")
@@ -59,10 +59,67 @@ class HBNBCommand(cmd.Cmd):
         argv[1] = argv[1].replace("\"", "")
         key = argv[0] + '.' + argv[1]
 
-        if al.get(key, False):
-            print(al[key])
+        if objects.get(key, False):
+            print(objects[key])
+        else:
+            print('** no instance found **')
+
+    def do_destroy(self, arg):
+        argv = arg.split()
+        if not argv:
+            print('** class name missing **')
+            return None
+        try:
+            eval(argv[0])
+        except:
+            print("** class doesn't exist **")
+            return None
+
+        objects = storage.all()
+
+        if len(argv) < 2:
+            print("** instance id missing **")
+            return None
+
+        argv[1] = argv[1].replace("\"", "")
+        key = argv[0] + '.' + argv[1]
+
+        if objects.get(key, False):
+            objects.pop(key)
+            storage.save()
         else:
             print("** no instance found **")
+
+    def do_all(self, arg):
+        pass
+
+    def do_update(self, arg):
+        args = arg.split()
+
+        if len(args) != 0:
+            if args[0] == "BaseModel":
+                if len(args) == 1:
+                    print('** instance id missing **')
+                else:
+                    key = args[0] + '.' + args[1]
+                    if key in storage.all():
+                        if len(args) > 2:
+                            if len(args) <= 3:
+                                print('** value missing **')
+                            else:
+                                setattr(
+                                    storage.all()[key],
+                                    args[2],
+                                    args[3][1:-1])
+                                storage.all()[key].save()
+                        else:
+                            print('** attribute name missing **')
+                    else:
+                        print('** no instance found **')
+            else:
+                print('** class doesn\'t exist **')
+        else:
+            print('** class name missing **')
 
 
 if __name__ == '__main__':
